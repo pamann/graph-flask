97e2bcf3d3bde86a1342f852882712cf5e99241c
Pam <beardsell.pamela@gmail.com>
2020-11-21 08:59:15 -0500

    See function to get search param from url arguments

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from flask import (Flask, render_template)
+# from pywikiapi import wikipedia as pywiki
+# import wikipedia
+# import time
+# import json
+# from concurrent.futures import ThreadPoolExecutor
+# from concurrent import futures
+# from multiprocessing import Pool
+# import hashlib
+from flask import request
+from flask import jsonify
+
+# from "./bfs_simple.py" import search_term
 
 app = Flask("__main__")
 
 @app.route("/")
 def my_index():
     return render_template("index.html", flask_token="Hello   world")
+
+@app.route("/see")
+def return_search():
+    search = request.args.get('search')
+    return jsonify(search)
+    # return search_term(search)
 
 app.run(debug=True)
