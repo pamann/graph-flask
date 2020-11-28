b6e8821b58542c3dd9aeebb59d8db7015f6ba0a8
Pam <beardsell.pamela@gmail.com>
2020-11-28 11:16:47 -0500

    Changing to serve index.html for /see endpont
    Rebase thread-descs onto development

--- bfs_simple.py
+++ bfs_simple.py
@@ -11,39 +11,49 @@ import hashlib
 
 nodes = set()
 links = set()
-site = pywiki('en')
-pool = ThreadPoolExecutor(8) # 8 threads, adjust to taste and # of cores
+site = pywiki("en")
+pool = ThreadPoolExecutor(8)  # 8 threads, adjust to taste and # of cores
 jobs = []
 views_dict = {}
 
+
 def query_wiki(ttls, tier):
     global site
-    for page in site.query(titles=ttls, format="json", pllimit="max", pvipdays=5, lhlimit="max", prop=["links", "linkshere", "description", "pageviews"], redirects=True):
+    for page in site.query(
+        titles=ttls,
+        format="json",
+        pllimit="max",
+        pvipdays=5,
+        lhlimit="max",
+        prop=["links", "linkshere", "description", "pageviews"],
+        redirects=True,
+    ):
         page = page.pages[0]
         return process_comp_jobs(page, tier, page.description, page.pageviews)
-    
+
 
 def process_comp_jobs(tt_page_s, tier, desc, views):
     global views_dict
-    if 'links' in tt_page_s and 'linkshere' in tt_page_s:
+    if "links" in tt_page_s and "linkshere" in tt_page_s:
         l = [v.title for v in tt_page_s.links]
         lh = [v.title for v in tt_page_s.linkshere]
         lset = set(l)
         lhset = set(lh)
         views = np.mean(list(views.values()))
         views_dict[tt_page_s.title] = views
-        
+
         # print(views)
-        if (tier == 2):
+        if tier == 2:
             tt_bidi_links = list(lset.intersection(lhset))
             tt_bidi_links = set(tt_bidi_links)
 
-        elif (tier == 1): 
+        elif tier == 1:
             tt_bidi_links = list(lset.intersection(lhset))
             tt_bidi_links = set(tt_bidi_links[0:10])
         aggregate_nodes(tt_bidi_links, tier, desc)
-        aggregate_links(tt_page_s.title, tt_bidi_links) 
+        aggregate_links(tt_page_s.title, tt_bidi_links)
         return tt_bidi_links
+
 
 def fetch_links(root_term):
     global nodes
@@ -55,37 +65,47 @@ def fetch_links(root_term):
     search_r = wikipedia.search(root_term)
     root = wikipedia.page(search_r[0])
     root_id = root.pageid
-    summary = root.summary.split('.')[0]
+    summary = root.summary.split(".")[0]
     count = 0
     aggregate_nodes([root_term], 3, summary)
 
     mean = np.mean(list(views_dict.values()))
     bidi_links = query_wiki(root_term, 2)
-    bidi_links = set(filter( lambda x: (views_dict[x[0]] > mean if x[0] in views_dict else 0) or x[1] == 1, nodes))
+    bidi_links = set(
+        filter(
+            lambda x: (views_dict[x[0]] > mean if x[0] in views_dict else 0)
+            or x[1] == 1,
+            nodes,
+        )
+    )
 
-
-    with ThreadPoolExecutor(8) as executor: # start threaded bidi links of second tier
+    with ThreadPoolExecutor(8) as executor:  # start threaded bidi links of second tier
         for bidi_link in bidi_links:
             jobs.append(executor.submit(query_wiki, bidi_link, 1))
         query_pool = Pool(processes=50)
-        tt_pool = [query_pool.apply_async(query_wiki, (p, 1))
-            for p in futures.as_completed(jobs)]
+        tt_pool = [
+            query_pool.apply_async(query_wiki, (p, 1))
+            for p in futures.as_completed(jobs)
+        ]
 
         query_pool.close()
         query_pool.terminate()
         query_pool.join()
-  
+
+
 def aggregate_links(nodeid, res):
     global links
     obj = [(nodeid, link_dest) for link_dest in res]
     links = links.union(set(obj))
 
-def aggregate_nodes(n_list, v, desc=''):
+
+def aggregate_nodes(n_list, v, desc=""):
     global nodes
-    if (v == 1):
-        desc = ''
+    if v == 1:
+        desc = ""
     obj = set([(node, v, desc) for node in n_list])
     nodes = nodes.union(set(obj))
+
 
 def search_term(search):
     global nodes
@@ -96,22 +116,22 @@ def search_term(search):
     # unpack sets of tuples into lists of dicts
     # print(views_dict)
     # print(nodes)
-    
+
     # print(len(nodes))
     # nodes = set(filter( lambda x: (views_dict[x[0]] > mean if x[0] in views_dict else 0) or x[1] == 1, nodes))
     # print(len(nodes))
-    l_nodes = [{
-        "name": name,
-        "id": hashlib.md5(name.encode('utf-8')).hexdigest(),
-        "val": val,
-        "description": desc,
-        "views": views_dict[name] if name in views_dict else 0
-    } for (name, val, desc) in nodes]
+    l_nodes = [
+        {
+            "name": name,
+            "id": hashlib.md5(name.encode("utf-8")).hexdigest(),
+            "val": val,
+            "description": desc,
+            "views": views_dict[name] if name in views_dict else 0,
+        }
+        for (name, val, desc) in nodes
+    ]
 
-    l_links = [{
-        "source": src,
-        "target": dest
-    } for (src, dest) in links]
+    l_links = [{"source": src, "target": dest} for (src, dest) in links]
 
-    graph = {'nodes': l_nodes, 'links': l_links}
+    graph = {"nodes": l_nodes, "links": l_links}
     return graph
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import (Flask, render_template)
+from flask import Flask, render_template
 from flask import request
 from flask import jsonify
 import wikipedia
@@ -7,20 +7,22 @@ from bfs_simple import search_term
 
 app = Flask("__main__")
 
+
 @app.route("/")
 def my_index():
     return render_template("index.html")
 
+
 @app.route("/see/<search>")
 def return_search(search):
-    search = search
-    return render_template("index.html", data=jsonify(search_term(search)))
-    # return jsonify(search_term(search))
+    return jsonify(search_term(search))
+
 
 @app.route("/meta/<term>")
 def metadata_fetch(term):
     page = wikipedia.page(term, preload=True)
-    res = { "title": page.title, "summary": page.summary, "image": page.images }
+    res = {"title": page.title, "summary": page.summary, "image": page.images}
     return jsonify(res)
+
 
 app.run(threaded=True, debug=True)
