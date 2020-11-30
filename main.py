b8619e1047bb9be381d5b1153eb6a328d8d5a163
Pam <beardsell.pamela@gmail.com>
2020-11-30 10:16:00 -0500

    Configured lint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import (Flask, render_template)
+from flask import Flask, render_template
 from flask import request
 from flask import jsonify
 
@@ -6,14 +6,15 @@ from bfs_simple import search_term
 
 app = Flask("__main__")
 
+
 @app.route("/")
 def my_index():
     return render_template("index.html")
 
-@app.route("/api/see/<search>", methods=['GET'])
+
+@app.route("/api/see/<search>", methods=["GET"])
 def return_search(search):
-    # print(search)
-    # print(search_term(search))
     return jsonify(search_term(search))
+
 
 app.run(debug=True)
